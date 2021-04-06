9d45d94a05eeebd9919d2780cd09e090faeca4e3
sinedied <noda@free.fr>
2021-04-06 17:50:04 +0200

    Update nav

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -3,18 +3,20 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = (props) => {
   return (
-    <nav className="column is-2 menu">
-      <p className="menu-label">Menu</p>
-      <ul className="menu-list">
-        <NavLink to="/products" activeClassName="active-link">
-          Products
-        </NavLink>
-        <NavLink to="/about" activeClassName="active-link">
-          About
-        </NavLink>
-      </ul>
-      {props.children}
-    </nav>
+    <div className="column is-2">
+      <nav className="menu">
+        <p className="menu-label">Menu</p>
+        <ul className="menu-list">
+          <NavLink to="/products" activeClassName="active-link">
+            Products
+          </NavLink>
+          <NavLink to="/about" activeClassName="active-link">
+            About
+          </NavLink>
+        </ul>
+        {props.children}
+      </nav>
+    </div>
   );
 };
 
