894a7fcfea835073a88c822e76501dbef4c5bf0a
sinedied <noda@free.fr>
2021-04-06 17:37:31 +0200

    Update base code

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = (props) => (
-  <nav className="column is-2 menu">
-    <p className="menu-label">Menu</p>
-    <ul className="menu-list">
-      <NavLink to="/products" activeClassName="active-link">
-        Products
-      </NavLink>
-      <NavLink to="/about" activeClassName="active-link">
-        About
-      </NavLink>
-    </ul>
-    {props.children}
-  </nav>
-);
+const NavBar = (props) => {
+  return (
+    <nav className="column is-2 menu">
+      <p className="menu-label">Menu</p>
+      <ul className="menu-list">
+        <NavLink to="/products" activeClassName="active-link">
+          Products
+        </NavLink>
+        <NavLink to="/about" activeClassName="active-link">
+          About
+        </NavLink>
+      </ul>
+      {props.children}
+    </nav>
+  );
+};
 
 export default NavBar;
