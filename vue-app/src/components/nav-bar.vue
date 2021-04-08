48087e097506b3202c71870ce455f4d4c3c75c14
sinedied <noda@free.fr>
2021-04-08 12:42:58 +0200

    Update vue navbar template

--- vue-app/src/components/nav-bar.vue
+++ vue-app/src/components/nav-bar.vue
@@ -8,11 +8,13 @@ export default {
 };
 </script>
 <template>
-  <nav class="column is-2 menu">
-    <p class="menu-label">Menu</p>
-    <ul class="menu-list">
-      <router-link to="/products">Products</router-link>
-      <router-link to="/about">About</router-link>
-    </ul>
-  </nav>
+  <div column is-2>
+    <nav class="menu">
+      <p class="menu-label">Menu</p>
+      <ul class="menu-list">
+        <router-link to="/products">Products</router-link>
+        <router-link to="/about">About</router-link>
+      </ul>
+    </nav>
+  </div>
 </template>
